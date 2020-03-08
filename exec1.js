6f7effa8c63cd7e5b8ac7d44ce1dd5708b008731
Alyson <alyson1907@gmail.com>
2020-03-08 17:33:17 -0300

    exec1: OK

--- exec1.js
+++ exec1.js
@@ -1,5 +1,12 @@
+/****************************************************************************** 
+**  Disciplina: SCC0270 - Redes Neurais e Aprendizado Profundo
+**  Nome: Alyson Matheus Maruyama Nascimento
+**  nUSP: 8532269
+*******************************************************************************/
+
 const fs = require('fs')
 const csv = require('csv-parser')
+const mathjs = require('mathjs')
 
 const parseValues = ({ V1, V2, V3 }) => {
   return {
@@ -22,18 +29,40 @@ const guessedRight = (result, expectedValue) => {
   return false
 }
 
-// Threshold function
+// Threshold activation function
 const threshold = (v) => {
   if (v >= 2) return 2
   else if (v < 2) return 1
 }
 
-let bestWeights
-let bestAccuracy = 0
+// Linear activation function
+const linear = (v) => {
+  const calculated = 0.1 * v
+  if (calculated >= 2) return 2
+  else if (calculated < 2) return 1
+}
+
+// Sigmoid activation function
+const sigmoid = (v) => {
+  const calculated = 1 / (1 + mathjs.exp(-v))
+  if (calculated >= 2) return 2
+  else if (calculated < 2) return 1
+}
+
+let bestWeights = {
+  threshold: [],
+  linear: [],
+  sigmoid: []
+}
+
+let bestAccuracy = {
+  threshold: 0,
+  linear: 0,
+  sigmoid: 0
+}
 
 const rows = []
 
-// Starting computation
 fs.createReadStream('Aula2-exec1.csv')
   .pipe(csv())
   .on('data', row => {
@@ -42,7 +71,11 @@ fs.createReadStream('Aula2-exec1.csv')
   })
   .on('end', () => {
     for (let i = 0; i < 700; i++) {
-      let rightGuesses = 0
+      let rightGuesses = {
+        threshold: 0,
+        linear: 0,
+        sigmoid: 0
+      }
       const weights = [Math.random(), Math.random() * 10]
       rows.forEach(row => {
         const { v1, v2, v3 } = parseValues(row)
@@ -50,21 +83,34 @@ fs.createReadStream('Aula2-exec1.csv')
 
         const v = calculateV(values, weights)
         // Classifying using threshold function
-        const result = threshold(v)
+        const thresholdResult = threshold(v)
+        const linearResult = linear(v)
+        const sigmoidResult = sigmoid(v)
 
-        if (guessedRight(result, v3)) {
-          rightGuesses++
+        // counting right classifications
+        if (guessedRight(thresholdResult, v3)) {
+          rightGuesses.threshold += 1
         }
-
-        const currentAccuracy = rightGuesses / rows.length
-        if (currentAccuracy >= bestAccuracy) {
-          bestAccuracy = currentAccuracy
-          bestWeights = weights
+        if (guessedRight(linearResult, v3)) {
+          rightGuesses.linear += 1
+        }
+        if (guessedRight(sigmoidResult, v3)) {
+          rightGuesses.sigmoid += 1
         }
       })
-    }
-    console.log(`Best accuracy (${bestAccuracy}) for threshold function was found with weights [${bestWeights}]`)
 
+      Object.entries(rightGuesses).forEach(([ functionName, rightGuesses ]) => {
+        const currentAccuracy = rightGuesses / rows.length
+        if (currentAccuracy >= bestAccuracy[functionName]) {
+          bestAccuracy[functionName] = currentAccuracy
+          bestWeights[functionName] = weights
+        }
+      })
+      
+    }
+    console.log(`Best accuracy (${bestAccuracy.threshold}) for threshold function was found with weights [${bestWeights.threshold}]`)
+    console.log(`Best accuracy (${bestAccuracy.linear}) for linear function was found with weights [${bestWeights.linear}]`)
+    console.log(`Best accuracy (${bestAccuracy.sigmoid}) for sigmoid function was found with weights [${bestWeights.sigmoid}]`)
   })
 
 
