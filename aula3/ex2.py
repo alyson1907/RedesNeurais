bed726f73a0acb721ee24cf28dfeffba600c47f7
Alyson <alyson1907@gmail.com>
2020-03-18 21:50:45 -0300

    fixed ex1: plotting 2 graphs at one, +ex2

--- aula3/ex2.py
+++ aula3/ex2.py
@@ -24,61 +24,54 @@ def parseColumns(dataset):
 
 # Accuracy calculation
 def accuracy(expected, predicted):
-	correct = 0
-	for i in range(len(expected)):
-		if expected[i] == predicted[i]:
-			correct += 1
-	return correct / float(len(expected)) * 100.0
+  correct = 0
+  for i in range(len(expected)):
+    if expected[i] == predicted[i]:
+      correct += 1
+  return correct / float(len(expected)) * 100.0
 
-# Threshold activation function
-def activationFuncThreshold(row, weights):
-  # bias
-	activation = weights[0]
-	for i in range(len(row)-1):
-		activation += weights[i + 1] * row[i]
-	return 2.0 if activation >= 0.0 else 1.0
-
-def activationFuncLinear(row, weights):
+# Sigmoid activation function
+def activationFuncSigmoid(row, weights):
   # bias
   activation = weights[0]
   for i in range(len(row)-1):
     activation += weights[i + 1] * row[i]
-  if activation <= 0.0:
-    return 1.0
-  elif activation > 0.0 and activation < 1.0:
-    return activation
-  else:
-    return 2.0
+  realValue = 1 / (1 + np.exp(-activation))
+  discrValue = 1.0 if realValue >= 1 else 2.0
+
+  return [realValue, discrValue]
 
 # Estimate Perceptron weights
 def trainWeightsPerceptron(trainingDataset, learningRate, epochs):
   # Initializing weights with random value
-  weights = [random.randint(0, 15) for i in range(len(trainingDataset[0]))]
+  weights = [random.randint(5, 15) for i in range(len(trainingDataset[0]))]
   errors = []
   for epoch in range(epochs):
     currentError = 0
     for row in trainingDataset:
-      prediction = activationFuncThreshold(row, weights)
+      realValue, discrValue = activationFuncSigmoid(row, weights)
+      prediction = discrValue
       error = prediction - row[-1]
       currentError += error**2
       # Updating new bias
-      weights[0] = weights[0] - (learningRate * error)
+      weights[0] = weights[0] + (learningRate * error)
       for i in range(len(row)-1):
-        weights[i + 1] = weights[i + 1] - (learningRate * row[i] * error)
-    errors.append(currentError)
-    print('Epoch=%d, learningRate=%.4f, currentError=%.4f' % (epoch, learningRate, currentError))
-    print('weights =', weights)
+        weights[i + 1] = weights[i + 1] + (learningRate * row[i] * error)
+    # print('Epoch=%d, learningRate=%.4f, currentError=%.4f' % (epoch, learningRate, currentError))
+    # print('weights =', weights)
     print('\n')
+    errors.append(currentError)
   return [weights, errors]
 
 def trainWeightsAdaline(trainingDataset, learningRate, epochs):
   # Initializing weights with random value
-  weights = [random.randint(0, 15) for i in range(len(trainingDataset[0]))]
+  weights = [random.randint(5, 15) for i in range(len(trainingDataset[0]))]
   errors = []
   for epoch in range(epochs):
     currentError = 0
     for row in trainingDataset:
-      prediction = activationFuncLinear(row, weights)
+      realValue, discrValue = activationFuncSigmoid(row, weights)
+      prediction = realValue
       error = row[-1] - prediction
       currentError += error**2
       # Updating new bias
@@ -87,26 +80,27 @@ def trainWeightsAdaline(trainingDataset, learningRate, epochs):
       for i in range(len(row)-1):
         weights[i + 1] = weights[i + 1] + (learningRate * row[i] * error)
     errors.append(currentError)
-    print('Epoch=%d, learningRate=%.4f, currentError=%.4f' % (epoch, learningRate, currentError))
-    print('weights =', weights)
-    print('\n')
+    # print('Epoch=%d, learningRate=%.4f, currentError=%.4f' % (epoch, learningRate, currentError))
+    # print('weights =', weights)
+    # print('\n')
+    print(len(errors))
   return [weights, errors]
 
-def Perceptron(trainDataset, testDataset, learningRate = 0.03, epochs = 70):
+def Perceptron(trainDataset, testDataset, learningRate = 0.03, epochs = 30):
   predictions = list()
   weights, errors = trainWeightsPerceptron(trainDataset, learningRate, epochs)
 
   for row in testDataset:
-    prediction = activationFuncThreshold(row, weights)
+    prediction = activationFuncSigmoid(row, weights)
     predictions.append(prediction)
   return [predictions, errors]
 
-def Adaline(trainDataset, testDataset, learningRate = 0.03, epochs = 70):
+def Adaline(trainDataset, testDataset, learningRate = 0.03, epochs = 30):
   predictions = list()
   weights, errors = trainWeightsAdaline(trainDataset, learningRate, epochs)
 
   for row in testDataset:
-    prediction = activationFuncLinear(row, weights)
+    prediction = activationFuncSigmoid(row, weights)
     predictions.append(prediction)
   return [predictions, errors]
 
@@ -121,15 +115,16 @@ for i in range(0, len(fullDataset), 3):
 
 perceptronPredictions, perceptronErrors = Perceptron(trainDataset, fullDataset)
 adalinePredictions, adalineErrors = Adaline(trainDataset, fullDataset)
-
+print(adalineErrors)
 # Calculating accuracy
 expectedArr = [row[-1] for row in fullDataset]
 print('Perceptron Accuracy: %.4f' % accuracy(expectedArr, perceptronPredictions))
 print('Adaline Accuracy: %.4f' % accuracy(expectedArr, adalinePredictions))
 
-plt.xlabel("Epoch")
-plt.ylabel("Error")
+plt.xlabel("Epoch") 
+plt.ylabel("Error") 
 plt.title("Perceptron(blue) | Adaline(red) Errors")
 plt.plot([index for index in range(len(perceptronErrors))], perceptronErrors)
+# To plot Adaline graph, uncomment lines below
 plt.plot([index for index in range(len(adalineErrors))], adalineErrors)
 plt.show()
